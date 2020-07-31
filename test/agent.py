aeadad958089abd752087478feca982bede7dc52
Michael Van Sint Jan <michael.vansintjan@woodtechms.com>
2020-07-31 11:27:22 -0400

    First commit

---
+++ test/agent.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+import time
+
+from hwagent.abstract_agent import AbstractHWAgent, AgentStatus, Message, HWStatus
+
+CONFIG_FILE = "config.yaml"
+
+
+class TestAgent(AbstractHWAgent):
+    def __init__(self):
+        AbstractHWAgent.__init__(self, CONFIG_FILE)
+        self.output_file_is_binary = False
+
+    def hw_run_data_threads(self):
+        """
+        Levanta los threads que reciben data del harwadre, la parsean y la escriben a disco
+        :return:
+        """
+        pass
+
+    def hw_stop_data_threads(self):
+        """
+        Termina los threads que reciben data del harwadre, la parsean y la escriben a disco
+        :return:
+        """
+        pass
+
+    def hw_start_streaming(self):
+        """
+        Inicia stream de datos desde el sensor
+        """
+        pass
+
+    def hw_stop_streaming(self):
+        """
+        Detiene el stream de datos desde el sensor
+        """
+        pass
+
+    def hw_connect(self):
+        time.sleep(2)
+        return True
+
+    def hw_reset_connection(self):
+        pass
+
+
+if __name__ == "__main__":
+    Path('logs').mkdir(exist_ok=True)
+    agent = TestAgent()
+    try:
+        agent.set_up()
+        agent.run()
+    except KeyboardInterrupt:
+        pass
---
+++ test/manager.py
@@ -0,0 +1,30 @@
+import socket
+import time
+from pathlib import Path
+
+from hwagent.abstract_agent import AgentStatus, Message, HWStatus
+
+agent_address = ('127.0.0.1', 30001)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setblocking(True)
+
+connected = False
+while not connected:
+    try:
+        sock.connect(agent_address)
+        connected = True
+    except ConnectionRefusedError:
+        time.sleep(0.1)
+
+dir = '\\mnt\\data\\capture\\002'
+Path(dir).mkdir(exist_ok=True)
+try:
+    while True:
+        sock.sendall(Message(Message.QUERY_AGENT_STATE).serialize())
+        print(sock.recv(1024).decode('ascii'))
+        time.sleep(1)
+        sock.sendall(Message(Message.SET_FOLDER, dir).serialize())
+except KeyboardInterrupt:
+    pass
+except Exception as e:
+    print(str(e))
